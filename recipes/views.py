515e91ed608fd13a79c7d1f299bb58bc395966e2
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-04-05 13:32:01 -0300

    Função render e caminho templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Rafael Amaral'
+    })
 
 
 def contato(request):
-    return HttpResponse('Contato1')
+    return render(request, 'recipes/contato.html')
 
 
 def sobre(request):
