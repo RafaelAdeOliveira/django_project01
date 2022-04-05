1d9c9dfece5ca32aca73bb8adfa563a4d2d20430
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-04-05 13:37:10 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Rafael Amaral'
-    })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre1')
+    return render(request, 'recipes/home.html')
