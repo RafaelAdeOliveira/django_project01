7ab48582d058c3621bd68b24fc403d55d9f6c6b4
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-04-07 18:53:47 -0300

    Filtrando is_published

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,9 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -13,8 +15,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
