b2e05d4deda1c14d2d86f9228d5fdf0476676074
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-04-11 19:28:23 -0300

    Usando self.client do Django para simular o cliente nos testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,8 +5,7 @@ from recipes import views
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
-        view = resolve(
-            reverse('recipes:home'))
+        view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
     def test_recipe_category_view_function_is_correct(self):
@@ -20,3 +19,11 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
