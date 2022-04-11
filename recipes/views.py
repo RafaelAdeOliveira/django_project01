57cb5f1168ad459c018feeba75c4995e054ea540
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-04-11 18:35:13 -0300

    Usando resolve para entender qual view function está sendo usada

--- recipes/views.py
+++ recipes/views.py
@@ -7,15 +7,19 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by('-id'))
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
