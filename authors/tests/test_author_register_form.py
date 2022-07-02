06d67cd6e5da8d1e4ff3820ab4c5a7accb17ae98
Rafael Amaral de Oliveira <rafael.amaraldeoliveira@gmail.com>
2022-07-02 19:25:11 -0300

    Criando testes para o formúlario - Começando com placeholder

---
+++ authors/tests/test_author_register_form.py
@@ -0,0 +1,18 @@
+from authors.forms import RegisterForm
+from django.test import TestCase
+from parameterized import parameterized
+
+
+class AuthorRegisterFormUnitTest(TestCase):
+    @parameterized.expand([
+        ('username', 'Your username'),
+        ('email', 'Your e-mail'),
+        ('first_name', 'Ex.: James'),
+        ('last_name', 'Ex.: Bonde'),
+        ('password', 'Type your password'),
+        ('password2', 'Repeat your password'),
+    ])
+    def test_first_name_placeholder_is_correct(self, field, placeholder):
+        form = RegisterForm()
+        current_placeholder = form[field].field.widget.attrs['placeholder']
+        self.assertEqual(placeholder, current_placeholder)
